3ee4d1978be24b1b1895b75b5ba7ad0e112ccb26
Luana Faria Lane <luanasfff@gmail.com>
2020-01-05 06:13:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -32,13 +32,4 @@ class Transfer
    end 
    
    
-   def reverse_transfer
-     if @status == "complete"
-      @sender.balance += amount
-      @receiver.balance -= amount
-      @status = "reversed"
-    end
- 
-end 
-  
-end
+   
