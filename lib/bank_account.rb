113dd7cda981ccb5b5b17180970496739a586eb8
Luana Faria Lane <luanasfff@gmail.com>
2020-01-05 06:19:33 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
 class BankAccount
 
-end
+  attr_accessor   :status , :balance 
+  attr_reader   :name 
+  
+  def initialize(name)
+    @name = name 
+    @balance = 1000 
+    @status = "open"
+    
+  end 
+
+   def deposit(money) 
+     @balance = @balance + money
+  end 
+  
+  
+   def display_balance
+      p "Your balance is $#{@balance}."
+   end
+
+   
+
+   def valid?
+    @status == "open" && @balance > 0
+   end
+  
+    def close_account
+      @status = "closed"
+    end
+    
+  end 
