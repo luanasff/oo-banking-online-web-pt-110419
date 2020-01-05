6a0bc1c45db99185177378a27d42b8e65efa0ab7
Luana Faria Lane <luanasfff@gmail.com>
2020-01-05 06:19:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,6 @@ class Transfer
       sender.balance -= @amount
       receiver.balance += @amount
       @status = 'complete'
-    #  @amount = 0
     else
       @status = 'rejected'
         'Transaction rejected. Please check your account balance.'
