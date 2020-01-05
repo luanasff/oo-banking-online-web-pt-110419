d86a5378676a599868c7660383a918dd128a1a31
Luana Faria Lane <luanasfff@gmail.com>
2020-01-05 06:17:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,6 +2,8 @@ class Transfer
 
    attr_reader  :sender , :receiver  , :amount
    attr_accessor :status
+   
+   
    def initialize(sender, receiver, amount)
      @sender = sender 
      @receiver = receiver 
