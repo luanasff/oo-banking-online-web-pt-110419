36cccd241202e4ad076c3bfcf6cb8f578bc488b9
Luana Faria Lane <luanasfff@gmail.com>
2020-01-05 05:53:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,14 @@
 class Transfer
-  # your code here
+
+   attr_accessor   :tranfer , :sender , :receiver , :status 
+   
+   def initialize(sender, receiver)
+     @sender = sender 
+     @receiver = receiver 
+     
+  end 
+
+ 
+
+
 end
