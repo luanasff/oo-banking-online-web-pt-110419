1cf3b152268bb6376237ed8aa43fd8e57da2c9ea
Luana Faria Lane <luanasfff@gmail.com>
2020-01-05 06:11:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,10 +18,17 @@ class Transfer
    def execute_transaction
        if valid? && status == "pending"
        if amount < self.sender.balance
-        self.sender.balance -= amount
-        self.receiver.balance += amount
-        self.status = "complete"
-       
+           self.sender.balance -= amount
+           self.receiver.balance += amount
+           self.status = "complete"
+       else
+         self.status = "rejected"
+         "Transaction rejected. Please check your account balance."
+       end
+       else
+         self.status = "rejected"
+           "Transaction rejected. Please check your account balance."
+        end
    end 
    
    
