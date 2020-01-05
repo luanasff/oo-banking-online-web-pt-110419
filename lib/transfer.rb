28dd657c551ee6cc0c3080b99a3f01be87992597
Luana Faria Lane <luanasfff@gmail.com>
2020-01-05 06:18:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,15 @@ class Transfer
    end
 
     
- 
+   def execute_transaction
+     if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      reject_transfer
+    end
+  end
     
    
 
