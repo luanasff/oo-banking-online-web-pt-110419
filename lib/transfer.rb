363bf23a4f243f34c23d37041489a02b40636911
Luana Faria Lane <luanasfff@gmail.com>
2020-01-05 06:10:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,5 +14,14 @@ class Transfer
       @sender.valid? && @receiver.valid? ? true : false
    end
 
-
+    
+   def execute_transaction
+  
+  
+   end 
+   
+   
+   def reverse_transfer
+     
+  end 
 end
